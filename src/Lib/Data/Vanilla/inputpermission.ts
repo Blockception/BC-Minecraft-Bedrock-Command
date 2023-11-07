132ef4795f88a09e9731f58bfcb579fddb2a4d4c
tryashtar <tryashtar@gmail.com>
2023-11-06 22:39:09 -0700

    Correct inputpermission parameter type
    entity (e.g. "minecraft:cow") -> selector (e.g. "@p")

--- src/Lib/Data/Vanilla/inputpermission.ts
+++ src/Lib/Data/Vanilla/inputpermission.ts
@@ -5,22 +5,22 @@ import { CommandInfo } from "../CommandInfo";
 export const inputpermission: CommandInfo[] = [
   {
     name: "inputpermission",
-    documentation: "Forces to open an NPC dialogue box to the targeted player(s)",
+    documentation: "Queries the status of an input permission",
     parameters: [
       { text: "inputpermission", type: ParameterType.keyword, required: true },
       { text: "query", type: ParameterType.keyword, required: true },
-      { text: "target", type: ParameterType.entity, required: true, options: { playerOnly: true } },
+      { text: "target", type: ParameterType.selector, required: true, options: { playerOnly: true } },
       { text: "permission", type: ParameterType.permission, required: true },
       { text: "state", type: ParameterType.permissionState, required: false },
     ],
   },
   {
     name: "inputpermission",
-    documentation: "Forces to open an NPC dialogue box to the targeted player(s)",
+    documentation: "Sets whether or not a player's input can affect their character",
     parameters: [
       { text: "inputpermission", type: ParameterType.keyword, required: true },
       { text: "set", type: ParameterType.keyword, required: true },
-      { text: "target", type: ParameterType.entity, required: true, options: { playerOnly: true } },
+      { text: "target", type: ParameterType.selector, required: true, options: { playerOnly: true } },
       { text: "permission", type: ParameterType.permission, required: true },
       { text: "state", type: ParameterType.permissionState, required: true },
     ],
