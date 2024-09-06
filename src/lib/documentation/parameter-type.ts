4bc11085196332d6e94a049ee02299510eb8af99
DaanV2 <daanverstraten@hotmail.com>
2024-09-06 17:14:24 +0200

    updating types

--- src/lib/documentation/parameter-type.ts
+++ src/lib/documentation/parameter-type.ts
@@ -1,6 +1,6 @@
 import { Modes } from "bc-minecraft-bedrock-types";
 import { ParameterType } from "../types/parameter-type";
-import { ModeHandler } from "bc-minecraft-bedrock-types/lib/src/modes/mode-handler";
+import { ModeHandler } from 'bc-minecraft-bedrock-types/lib/modes/mode-handler';
 
 export const ParameterTypeDocumentation: Partial<Record<ParameterType, string>> = {
   [ParameterType.animation]: `## Animation
