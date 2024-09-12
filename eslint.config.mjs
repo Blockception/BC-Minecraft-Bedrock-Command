d95f713f931f2e9148ca6d662ce982042d6ed86f
DaanV2 <daanverstraten@hotmail.com>
2024-09-12 14:53:42 +0200

    updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,16 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   {
     files: ["**/*.ts"],
-    ignores: ["coverage/**/*", "node_modules/**/*"],
+    ignores: [
+      "coverage/",
+      "coverage/*",
+      "coverage/**/*",
+      "lib/",
+      "lib/*",
+      "lib/**/*",
+      "node_modules/",
+      "node_modules/*",
+    ],
     extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
     plugins: {
       jest: {},
